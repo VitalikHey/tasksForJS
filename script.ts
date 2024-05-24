554a1113ff3e07a1d76169cf6a4fc1e514d30e62
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-05-24 13:08:53 +0300

    Add three tasks for typescript

--- script.ts
+++ script.ts
@@ -1,77 +1,77 @@
-// Убрал экспорты иначе браузер ощибку выдаёт
-
-// Задание 1
-
-interface User {
-    name: string,
-    age: number,
-    occupation: string
-}
-
-const users: User[] = [
-    {
-        name: 'Max Mustermann',
-        age: 25,
-        occupation: 'Chimney sweep'
-    },
-    {
-        name: 'Kate Müller',
-        age: 23,
-        occupation: 'Astronaut'
-    }
-];
-
-function logPerson(user: User) {
-    console.log(` - ${user.name}, ${user.age}`);
-}
-
-console.log('Users:');
-users.forEach(logPerson);
-
-// Задание 2
-
-interface User {
-    name: string;
-    age: number;
-    occupation: string;
-}
-
-interface Admin {
-    name: string;
-    age: number;
-    role: string;
-}
-
-type Person = User | Admin
-
-const persons: Person[] = [
-    {
-        name: 'Max Mustermann',
-        age: 25,
-        occupation: 'Chimney sweep'
-    },
-    {
-        name: 'Jane Doe',
-        age: 32,
-        role: 'Administrator'
-    },
-    {
-        name: 'Kate Müller',
-        age: 23,
-        occupation: 'Astronaut'
-    },
-    {
-        name: 'Bruce Willis',
-        age: 64,
-        role: 'World saver'
-    }
-];
-
-function logPerson(user: Person) {
-    console.log(` - ${user.name}, ${user.age}`);
-}
-
-persons.forEach(logPerson);
+// // Убрал экспорты иначе браузер ощибку выдаёт
+//
+// // Задание 1
+//
+// interface User {
+//     name: string,
+//     age: number,
+//     occupation: string
+// }
+//
+// const users: User[] = [
+//     {
+//         name: 'Max Mustermann',
+//         age: 25,
+//         occupation: 'Chimney sweep'
+//     },
+//     {
+//         name: 'Kate Müller',
+//         age: 23,
+//         occupation: 'Astronaut'
+//     }
+// ];
+//
+// function logPerson(user: User) {
+//     console.log(` - ${user.name}, ${user.age}`);
+// }
+//
+// console.log('Users:');
+// users.forEach(logPerson);
+//
+// // Задание 2
+//
+// interface User {
+//     name: string;
+//     age: number;
+//     occupation: string;
+// }
+//
+// interface Admin {
+//     name: string;
+//     age: number;
+//     role: string;
+// }
+//
+// type Person = User | Admin
+//
+// const persons: Person[] = [
+//     {
+//         name: 'Max Mustermann',
+//         age: 25,
+//         occupation: 'Chimney sweep'
+//     },
+//     {
+//         name: 'Jane Doe',
+//         age: 32,
+//         role: 'Administrator'
+//     },
+//     {
+//         name: 'Kate Müller',
+//         age: 23,
+//         occupation: 'Astronaut'
+//     },
+//     {
+//         name: 'Bruce Willis',
+//         age: 64,
+//         role: 'World saver'
+//     }
+// ];
+//
+// function logPerson(user: Person) {
+//     console.log(` - ${user.name}, ${user.age}`);
+// }
+//
+// persons.forEach(logPerson);
 
 // Задание 3
 
@@ -111,22 +111,15 @@ const persons: Person[] = [
         role: 'World saver'
     }
 ];
-
-function isUser(person: Person): person is User {
-    if (person.age < 30) {
-        return true;
-    }
-    return false;
-}
-
 function logPerson(person: Person) {
     let additionalInformation: string;
-    if (!isUser(person)) {
+    if ("role" in person) {
         additionalInformation = person.role;
     } else {
         additionalInformation = person.occupation;
     }
     console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
 }
+
 
 persons.forEach(logPerson);
